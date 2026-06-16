844a254c6c61c9f6300b6435d5fb62a5ac4eb077
LySovanra123 <157804051+LySovanra123@users.noreply.github.com>
2026-06-16 13:11:09 +0700

    destructuring prop

---
+++ app/destructuring-prop/page.tsx
@@ -0,0 +1,5 @@
+import DestructuringProp from "@/components/destructuring-prop/destructuring-prop-page"
+
+export default function Page(){
+    return <DestructuringProp id={1} name="Vanra"/>
+}
---
+++ components/destructuring-prop/destructuring-prop-page.tsx
@@ -0,0 +1,10 @@
+const DestructuringProp = ({ id, name }: { id: number, name: string }) => {
+    return (
+        <div>
+            <h1>ID:{id}</h1>
+            <p>Name:{name}</p>
+        </div>
+    )
+}
+
+export default DestructuringProp
